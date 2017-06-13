e186310d467ec4ef72157a4fd5f8453daaa7d714
Max Jacobson <max@hardscrabble.net>
2017-06-12 20:23:45 -0400

    start comparing across implementation...

--- spec/our/enumerator_spec.rb
+++ spec/our/enumerator_spec.rb
@@ -1,15 +1,31 @@
 require "our/enumerator"
 
-describe Our::Enumerator do
-  it "works" do
-    enum = described_class.new do |yielder|
-      i = 0
-      loop do
-        yielder.yield(i)
-        i += 1
+[Our::Enumerator, ::Enumerator].each do |klass|
+  describe klass do
+    subject {
+      described_class.new do |yielder|
+        i = 0
+        loop do
+          yielder.yield(i)
+          i += 1
+        end
+      end
+    }
+
+    describe "#next" do
+      it "gives the next item" do
+        expect(subject.next).to eq(0)
+        expect(subject.next).to eq(1)
+        expect(subject.next).to eq(2)
+        expect(subject.next).to eq(3)
       end
     end
 
-    expect(enum.take(3)).to eq([0, 1, 2])
+    describe "#take" do
+      it "takes the first n yielded items" do
+        expect(subject.take(3)).to eq([0, 1, 2])
+        expect(subject.take(3)).to eq([0, 1, 2])
+      end
+    end
   end
 end
